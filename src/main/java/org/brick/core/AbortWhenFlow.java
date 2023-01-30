4c3c44dadac89176a594ba1d89163dc7bdf5c1a0
Jay <janlely@163.com>
2023-01-30 14:13:36 +0800

    update

--- src/main/java/org/brick/core/AbortWhenFlow.java
+++ src/main/java/org/brick/core/AbortWhenFlow.java
@@ -1,6 +1,5 @@
 package org.brick.core;
 
-import net.jodah.typetools.TypeResolver;
 import org.apache.commons.lang3.ClassUtils;
 import org.brick.types.Either;
 
@@ -23,7 +22,7 @@ public class AbortWhenFlow<I,O,C> implements SubFlow.ISubFlow<I, Either<I,O>,C> 
     @Override
     public FlowDoc<I, Either<I,O>, C> getFlowDoc() {
         FlowDoc<I,Either<I,O>,C> flowDoc = new FlowDoc<>(this.desc, FlowType.ABORT, getFlowName());
-        Class<?>[] classes = TypeResolver.resolveRawArguments(AbortWhenFlow.class, this.getClass());
+//        Class<?>[] classes = TypeResolver.resolveRawArguments(AbortWhenFlow.class, this.getClass());
         flowDoc.add(this.abortFlow.getFlowDoc());
 //        return flowDoc.types((Class<I>) classes[0], (Class<Either<I,O>>) classes[1], (Class<C>) classes[2]);
         return flowDoc;
